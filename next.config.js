2c128ed49e0ca27682557dd3ceda0445fca4814c
yangtopia <yangtopia@gmail.com>
2020-04-14 23:04:02 +0900

    KEY

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 const isDev = process.env.NODE_ENV !== 'production';
 
+require('dotenv').config();
+
 module.exports = {
   distDir: isDev ? '.next' : 'dist/functions/next',
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  },
   webpack: (config) => {
     return config;
   },
